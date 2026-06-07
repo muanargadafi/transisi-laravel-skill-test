b13388cfba2033d4e04470dd050a76c5d53d907b
Muanar Gadafi <muanar.gadafi@gmail.com>
2026-06-07 11:24:59 +0700

    chore: install spatie/laravel-medialibrary and run media migrations

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,19 @@ return [
             'report' => false,
         ],
 
+        'media' => [
+            'driver' => 'local',
+            'root' => public_path('media'),
+            'url' => env('APP_URL').'/media',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'company_disk' => [
+            'driver' => 'local',
+            'root' => storage_path('app/company'), // Mengarah langsung ke storage/app/company
+            'throw' => false,
+        ],
     ],
 
     /*
