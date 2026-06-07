cba4a50e31a9e86f48671ef575e271ec0f71b90b
Muanar Gadafi <muanar.gadafi@gmail.com>
2026-06-07 08:43:07 +0700

    feat: add admin authentication and database seeder for admin user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin Transisi',
+            'email' => 'admin@transisi.id',
+            'password' => Hash::make('transisi'),
+        ]);
+    }
+}
